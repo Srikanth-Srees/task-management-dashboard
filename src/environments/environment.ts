c0b6484bec184d78129f61a1b71bffadde919ef1
Srikanth Bestha <tonysrikanth500@gmail.com>
2026-06-18 09:47:50 +0530

    docs: revert environment setup wording to be more descriptive

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  apiUrl: '', // Set your API URL here. If empty, the default will be used.
+  apiUrl: 'https://jsonplaceholder.typicode.com/todos', // Set your API URL here. If empty, the default will be used.
 };
